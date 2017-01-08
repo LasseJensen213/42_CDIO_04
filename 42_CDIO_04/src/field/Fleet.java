e2577d25c3e44c06ab9d3b22d0aa49e6d2fe1565
Jimmy <Jimmy@Jimmy-Pc>
2017-01-09 00:03:19 +0100

    Andreas's LandOnField

--- 42_CDIO_04/src/field/Fleet.java
+++ 42_CDIO_04/src/field/Fleet.java
@@ -1,10 +1,8 @@
 package field;
 
-import java.awt.Image;
+public class Fleet extends Ownable{
 
-public class Fleet extends Field{
-
-	public Fleet(String desc, String subtext, String title, int pos) {
-		super(desc,subtext,title,pos);
+	public Fleet(String desc, String subtext, String title, int pos, int price, int rent) {
+		super(desc,subtext,title,pos, price, rent);
 	}
 }
---
+++ 42_CDIO_04/src/field/LandOnFieldController.java
@@ -0,0 +1,131 @@
+package field;
+
+import board.Board;
+import boundary.LandOnFieldBoundary;
+import player.Player;
+
+public class LandOnFieldController {
+
+
+	public void landOnField(Player player){
+		Board b = new Board();
+		field.Field f = b.getField(player.getPlayerPos());
+
+		if(f instanceof Territory){
+			landOnTerritory(player,(Territory)f);
+		}
+		else if(f instanceof Brewery){
+			landOnBrewery(player,(Brewery)f);
+		}
+		else if(f instanceof Fleet){
+			landOnFleet(player,(Fleet)f);
+		}
+		else if(f instanceof Chance){
+			landOnChance(player,(Chance)f);
+		}
+		else if(f instanceof GoToJail){
+			landOnGoToJail(player,(GoToJail)f);
+		}
+		else if(f instanceof Jail){
+			landOnJail(player,(Jail)f);
+		}
+		else if(f instanceof ParkingLot){
+			landOnParkingLot(player);
+		}
+		else if(f instanceof Start){
+			landOnStart(player,(Start)f);
+		}
+		else if(f instanceof Tax){
+			landOnTax(player,(Tax)f);
+		}
+	}
+
+	// mangler udregning af leje
+	public void landOnTerritory(Player player, Territory f){
+		if(f.getOwner() == null){
+			if(player.getAccount().getBalance()>(f.getPrice())){
+				if(LandOnFieldBoundary.buyChoice()){
+					player.getAccount().withdraw(f.getPrice());
+					f.setOwner(player);
+				}
+			}
+		}
+		else if(f.getOwner() == player){
+			//nothing happens
+		}
+		else {
+			// mangler metode til at trække dobbelt op på husleje og huse. 1000 er et midlertidigt tal.
+
+			player.getAccount().transfer(1000, f.getOwner().getAccount());
+		}
+	}
+	// mangler udregning af leje
+	public void landOnBrewery(Player player, Brewery f){
+		if(f.getOwner() == null){
+			if(player.getAccount().getBalance()>(f.getPrice())){
+				if(LandOnFieldBoundary.buyChoice()){
+					player.getAccount().withdraw(f.getPrice());
+					f.setOwner(player);
+				}
+			}
+		}
+		else if(f.getOwner() == player){
+			//nothing happens
+		}
+		else {
+			// mangler metode til at trække dobbelt op på husleje og huse. 1000 er et midlertidigt tal.
+
+			player.getAccount().transfer(1000, f.getOwner().getAccount());
+		}
+	}
+	// mangler udregning af leje
+	public void landOnFleet(Player player, Fleet f){
+		if(f.getOwner() == null){
+			if(player.getAccount().getBalance()>(f.getPrice())){
+				if(LandOnFieldBoundary.buyChoice()){
+					player.getAccount().withdraw(f.getPrice());
+					f.setOwner(player);
+				}
+			}
+		}
+		else if(f.getOwner() == player){
+			//nothing happens
+		}
+		else {
+			// mangler metode til at trække dobbelt op på husleje og huse. 1000 er et midlertidigt tal.
+
+			player.getAccount().transfer(1000, f.getOwner().getAccount());
+		}
+	}
+	//færdig
+	public void landOnTax(Player player, Tax f){
+		player.getAccount().withdraw(f.getTax());
+		ParkingLot.setTaxMoney(ParkingLot.getTaxMoney()+f.getTax());
+	}
+	//færdig
+	public void landOnParkingLot(Player player){
+		player.getAccount().deposit(ParkingLot.getTaxMoney());
+		ParkingLot.setTaxMoney(0);
+	}
+	//færdig
+	public void landOnJail(Player player, Jail f){
+
+	}
+	//færdig
+	public void landOnGoToJail(Player player, GoToJail f){
+
+		LandOnFieldBoundary.moveToJail(player.getName(), player.getPlayerPos());
+		player.setPlayerPos(10);
+		player.setJailed(true);
+
+	}
+	//ikke lavet
+	public void landOnStart(Player player, Start f){
+	}
+	//ikke lavet
+	public void landOnChance(Player player, Chance f){
+
+	}
+	
+	
+}
