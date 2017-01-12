825b6c136492b8247539095256be8b9666ebc2bc
Jimmy <Jimmy@Jimmy-Pc>
2017-01-12 11:44:47 +0100

    Move to nearest fleet

--- 42_CDIO_04/src/main/Main.java
+++ 42_CDIO_04/src/main/Main.java
@@ -8,7 +8,7 @@ public class Main {
 	
 	public static void main(String[] args)
 	{
-		System.out.println((1)%-40);
+		System.out.println(-41%40);
 		MainMenuController menu = new MainMenuController();
 		menu.MainMenu();
 	}
