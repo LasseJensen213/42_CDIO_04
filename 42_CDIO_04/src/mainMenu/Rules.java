0532889c5fe1ca6d3e3a2bb6977220d9798c5e6e
Jimmy <Jimmy@Jimmy-Pc>
2017-01-13 21:43:31 +0100

    rules

--- 42_CDIO_04/src/mainMenu/Rules.java
+++ 42_CDIO_04/src/mainMenu/Rules.java
@@ -4,11 +4,8 @@ import java.io.IOException;
 
 public class Rules {
 
-<<<<<<< HEAD
 	private String fileName = System.getProperty("user.dir")+"/Dokumentation/Regler.txt";
-=======
-	private final String fileName = "resources/rules/Regler.txt";
->>>>>>> branch 'develop' of https://github.com/LasseJensen213/42_CDIO_04
+
 
 	public void open()
 	{
